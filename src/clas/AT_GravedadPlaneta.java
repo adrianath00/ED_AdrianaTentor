3ecbfe070d4da28f5dee3578e7f252bd763ee71d
adrianath00 <adrianatentor@hotmail.com>
2025-04-30 19:09:31 +0200

    AT_Commit de la segunda clase GravedadPlaneta

--- src/clas/AT_GravedadPlaneta.java
+++ src/clas/AT_GravedadPlaneta.java
@@ -1,5 +1,50 @@
 package clas;
 
 public class AT_GravedadPlaneta {
+	private int id;
+	private String nombre;
+	private double GravedadPlaneta;
+	
+	//Constructor por defecto.
+	public AT_GravedadPlaneta() {
+		super();
+	}
+	
+	// Constructor con todos los argumentos. GPT no incluyas el id
+	public AT_GravedadPlaneta(String nombre, double gravedadPlaneta) {
+		super();
+		this.nombre = nombre;
+		GravedadPlaneta = gravedadPlaneta;
+	}
+	
+	// Métodos getter y setter.
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getGravedadPlaneta() {
+		return GravedadPlaneta;
+	}
+
+	public void setGravedadPlaneta(double gravedadPlaneta) {
+		GravedadPlaneta = gravedadPlaneta;
+	}
+	
+	// Método ToString.
+	@Override
+	public String toString() {
+		return "AT_GravedadPlaneta [id=" + id + ", nombre=" + nombre + ", GravedadPlaneta=" + GravedadPlaneta + "]";
+	}
 }
